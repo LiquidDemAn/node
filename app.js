dceeb54fb712b5478ad36ce0fca560c855281f02
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-03-01 21:58:42 +0200

    Setup easy server

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require('http')
+const routes = require('./routes')
 
-const server =  http.createServer((req, res) => {
-    console.log(req)
-})
+http.createServer(routes).listen(3000)
 
-server.listen(3000)
