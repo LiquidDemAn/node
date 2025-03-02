d7a216b038a5219da4c50066d9b7c950c9a5eb71
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-03-02 22:08:44 +0200

    Practice with express

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const http = require('http')
-const routes = require('./routes')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-http.createServer(routes).listen(3000)
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// ROUTES
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
