28e3182fac94b04c9ca021ea9e98d61ffa738758
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-03-20 21:35:03 +0200

    Add mvc, split routes, add product model

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res) => {
+  const product = new Product(req.body.title);
+
+  product.save();
+
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res) => {
+  const products = Product.fetchAll();
+
+  res.render("shop", {
+    pageTitle: "Shop",
+    products,
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,18 +1,9 @@
 const express = require("express");
 
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("shop", {
-    pageTitle: "Shop",
-    products: adminData.products,
-    path: "/",
-    hasProducts: adminData.products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
